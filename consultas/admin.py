c9e91f5cb1c1ff322fdb18d9ff5cad953755acb7
luismuhele <luismuhele@gmail.com>
2024-09-30 11:48:55 +0100

    redefinindo os títulos do painel administrativo

--- consultas/admin.py
+++ consultas/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 
 from .models import Consulta, TipoConsulta
 
+admin.site.site_header = "Painel Administrativo - Clínica Visão Futurista"
+admin.site.site_title = "Painel Administrativo - Clínica Visão Futurista"
+admin.site.index_title = "Bem-vindo ao Painel de Administração"
+
 admin.site.register(TipoConsulta)
 admin.site.register(Consulta)
